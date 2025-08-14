cd045ea64f24bb05bf52d6b2e691a31c14773e2e
Matías Ezequiel Wasyluk <mataiasa.d@gmail.com>
2025-08-14 14:44:04 -0300

    feat(ALP#15): implementa html parciales
    Se compila con gulp en una carpeta dist un html con las distintas secciones importadas dentro de un index

--- gulpfile.js
+++ gulpfile.js
@@ -5,42 +5,56 @@ const rename = require("gulp-rename");
 const sourcemaps = require("gulp-sourcemaps");
 const terser = require("gulp-terser");
 const browserSync = require("browser-sync").create();
+const fileInclude = require('gulp-file-include');
 
-//compile scss
+// HTML con includes (sections)
+function html() {
+  return gulp.src(['src/*.html', '!src/sections/**'])
+    .pipe(fileInclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+}
+
+// SCSS → CSS minificado
 function styles() {
   return gulp.src("./scss/main.scss")
     .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./css"))
+    .pipe(gulp.dest("./dist/css"))
     .pipe(cleanCSS())
     .pipe(rename({ suffix: ".min" }))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("./css"))
+    .pipe(gulp.dest("./dist/css"))
     .pipe(browserSync.stream());
 }
 
-//minify js
+// JS minificado
 function scripts() {
   return gulp.src("./js/**/*.js")
     .pipe(sourcemaps.init())
     .pipe(terser())
     .pipe(rename({ suffix: ".min" }))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("./js/min"))
+    .pipe(gulp.dest("./dist/js/min"))
     .pipe(browserSync.stream());
 }
 
-//dev server
+// Servidor local y watcher
 function serve() {
   browserSync.init({
-    server: { baseDir: "./" }
+    server: { baseDir: "./dist" }
   });
+
+  gulp.watch("src/**/*.html", html);
   gulp.watch("./scss/**/*.scss", styles);
   gulp.watch("./js/**/*.js", scripts);
-  gulp.watch("./*.html").on("change", browserSync.reload);
 }
 
+exports.html = html;
 exports.styles = styles;
 exports.scripts = scripts;
-exports.serve = gulp.series(styles, scripts, serve);
-exports.default = gulp.series(styles, scripts, serve);
+exports.serve = gulp.series(html, styles, scripts, serve);
+exports.default = gulp.series(html, styles, scripts, serve);
