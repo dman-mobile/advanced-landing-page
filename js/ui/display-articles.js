cdea52e5b83d261eb117edf777b2b2f8f7549e38
Matías Ezequiel Wasyluk <mataiasa.d@gmail.com>
2025-08-26 12:03:40 -0300

    feat(ALP#23): implementa mostrar 3 nuevos articulos

---
+++ js/ui/display-articles.js
@@ -0,0 +1,42 @@
+export default function initDisplayArticles(){
+  const findMoreBtn = document.getElementById('find-more__button');
+  const articlesList = document.querySelector('.articles__list');
+
+  findMoreBtn.addEventListener('click', async () => {
+    let response = await fetch('data/articles.json');
+    let data = await response.json();
+
+    let randomArticles = data.articles
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 3);
+
+    articlesList.innerHTML = "";
+
+    randomArticles.forEach(article => {
+      articlesList.innerHTML += `
+        <article class="article-card">
+          <div class="article-card__image">
+            <img src="${article.image}" alt="${article.title}">
+          </div>
+          <div class="article-card__content">
+            <div class="article-card__author">
+              <img src="${article.authorImage}" alt="${article.author}" class="article-card__author-image">
+              <span class="article-card__author-name">${article.author}</span>
+            </div>
+            <h5 class="article-card__title">
+              ${article.title}
+            </h5>
+            <div class="article-card__meta">
+              <svg fill="none" height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <use href="assets/icons/icons.svg#icon-clock"></use>
+              </svg>
+              <span class="article-card__meta-time">${article.readTime}</span>
+              |
+              <span class="article-card__meta-date">${article.date}</span>
+            </div>
+          </div>
+        </article>
+      `;
+    });
+  });
+}
